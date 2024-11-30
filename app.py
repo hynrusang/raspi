9b8f985f4c2d68c85b40ad6f2c16996f51f366ed
hynrusang <84229112+hynrusang@users.noreply.github.com>
2024-11-30 21:50:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,33 +12,50 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 camera = cv2.VideoCapture(0)
 
+lock = 0
 ledPin = 6
+ledMode = "수동"
+ledState = "off"
+data = {
+    "temp": 0
+    "humi": 0
+    "light": 0
+}
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(ledPin, GPIO.OUT)
 sensor = HTU21D(busio.I2C(3, 2))
 mcp = Adafruit_MCP3008.MCP3008(clk=11, cs=8, miso=9, mosi=10)
 
-ledMode = "수동"
-ledState = "off"
-
 if not camera.isOpened():
     print("카메라를 열 수 없습니다.")
     exit()
 
-def measure(target, dataSource):
-    if (target == "temp"):
-        return round(float(dataSource.temperature), 1)
-    elif (target == "humi"):
-        return round(float(dataSource.relative_humidity), 1)
+def syncLedState():
+    return
 
 def sendInfo():
     while True:
-        socketio.emit("onInfo", {"message": f"조도: {mcp.read_adc(0)}, 온도: {measure('temp', sensor)}, 습도: {measure('humi', sensor)}"})
+        data.temp = round(float(sensor.temperature), 1)
+        data.humi = round(float(sensor.relative_humidity), 1)
+        data.light = float(mcp.read_adc(0) / 10)
+        socketio.emit("onInfo", {"message": f"조도: {data.light}, 온도: {data.temp}, 습도: {data.humi}"})
         socketio.sleep(1)
 
 @socketio.on("connect")
 def initSocket():
-    socketio.start_background_task(sendInfo)
+    global lock
+
+    if lock == 0:  # 다른 클라이언트가 없으면 연결 허용
+        lock = 1
+        socketio.start_background_task(sendInfo)
+    else: disconnect()
+
+@socketio.on("disconnect")
+def handle_disconnect():
+    global lock
+    if lock == 1: lock = 0
+
 
 @socketio.on('ePhotoRequest')
 def requestPhoto():
